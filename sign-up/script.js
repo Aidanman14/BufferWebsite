fea891207fc49b882cd868dce2ba0507d4791c3a
Rice <104523156+Aidanman14@users.noreply.github.com>
2026-04-20 18:26:19 +0100

    Update script.js

--- sign-up/script.js
+++ sign-up/script.js
@@ -1,6 +1,11 @@
 let projectKey = "BUFFERSTAKEHOLDERINVITE2026-27";
 let password = "";
 
+// vatsal's key: 6ac7f00933165014d1db908e1181ffa75bc7298e8143aa61062b0a08ac6366c0
+// daniel's key: bd3dae5fb91f88a4f0978222dfd58f59a124257cb081486387cbae9df11fb879
+// mama's key: ccb711f092ac8ef1805b5045fab7e8a6189cb97ad04565e21b5fbcfc9e542e42
+// tahmasbi's key: 6e3da5b1aace18618f33ada2fbcf5304c7d773bb7906b17d5331286a198e2473
+
 function checkCredentials(email, user, password, key) {
     if ((!email || email.length === 0 || /^\s*$/.test(email)) || (!user || user.length === 0 || /^\s*$/.test(user)) || (!password || password.length === 0 || /^\s*$/.test(password)) || (!key || key.length === 0 || /^\s*$/.test(key))) { return false; }
     return true;
@@ -32,6 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
         
         if (!(key == projectKey)) { alert("Invalid invite key!"); return; }
 
-        alert("Signed up ez noob.");
+        alert(`Signed up.\nEmail:${email}\nUsername:${user}\nPassword:${password}\nInvite key used:${key}`);
     }
 })

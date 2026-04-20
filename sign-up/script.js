0e917e83113147d5f5b1ae3fe133ff48e15b9348
Rice <104523156+Aidanman14@users.noreply.github.com>
2026-04-20 17:45:55 +0100

    Update script.js

--- sign-up/script.js
+++ sign-up/script.js
@@ -1,4 +1,5 @@
 let projectKey = "BUFFERSTAKEHOLDERINVITE2026-27";
+let password = "";
 
 function checkCredentials(email, user, password, key) {
     if ((!email || email.length === 0 || /^\s*$/.test(email)) || (!user || user.length === 0 || /^\s*$/.test(user)) || (!password || password.length === 0 || /^\s*$/.test(password)) || (!key || key.length === 0 || /^\s*$/.test(key))) { return false; }
@@ -6,10 +7,23 @@ function checkCredentials(email, user, password, key) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("pass").onkeyup = function() {
+        password = this.value;
+        let length = text.length;
+        let censored = "";
+
+        for (let i = 0; i < length; i++) {
+            censored = censored + "•";
+        }
+
+        if (this.value !== censored) {
+                this.value = censored;
+        }
+    }
+
     document.getElementById("signupbtn").onclick = function() {
         let email = document.getElementById("email").value;
         let user = document.getElementById("user").value;
-        let password = document.getElementById("pass").value;
         let key = document.getElementById("key").value;
 
         let check = checkCredentials(email, user, password, key);
